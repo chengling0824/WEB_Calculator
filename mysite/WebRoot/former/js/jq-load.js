af018cb39715473499209f77904c96a1529f1927
dandelion <dandelion18@sina.cn>
2018-05-16 12:49:09 +0800

    delete

--- mysite/WebRoot/former/js/jq-load.js
+++
@@ -1,8 +0,0 @@
-$('nav a #next').on('click', function(e) {                 
-  e.preventDefault();  // 阻止链接跳转
-  var url = this.href;  // 保存点击的地址     
-  var id = "<%session.getAttribute(\"id\");"
-
-  $('#container').remove();                          
-  $('#content').load(url + ' #container').fadeIn('slow'); // 加载新内容,url地址与该地址下的选择器之间要有空格,表示该url下的#container
-});
